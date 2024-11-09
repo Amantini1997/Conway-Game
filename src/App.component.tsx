dd35fb3a4a498bc85b58f2be497ebffe5bdd6e93
Alessandro Amantini <ale.amantini1997@gmail.com>
2024-11-09 17:06:18 +0000

    clean code

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -5,15 +5,11 @@ import { Board } from './board/board.component';
 import { Background, Title } from './App.styles';
 
 const NO_BOARD_LOADED = -2;
-const BOARD_NOT_INITIALISED = -1;
 
 export const App = () => {
 	const boardStateHistory = useRef<IBoardState[] | null>(null);
 	const boardSize = useRef<IBoardSize>();
 	const [currentBoardTime, setCurrentBoardTime] = useState(NO_BOARD_LOADED);
-	// const boardStateHistory = useRef<IBoardState[] | null>([]);
-	// const boardSize = useRef<IBoardSize>({ cols: 3, rows: 3 });
-	// const [currentBoardTime, setCurrentBoardTime] = useState(BOARD_NOT_INITIALISED);
 	const [currentBoardBoard, setCurrentBoard] = useState<IBoardState>([]);
 
 	const isBoardLoaded = currentBoardTime !== NO_BOARD_LOADED;
